24fd4b970fdb98206412113c38487873c482b159
Ruth <ruthjohnson2013@gmail.com>
2017-01-12 14:01:55 -0800

    removed old CANVIS files

--- CANVIS/CANvis_Sample/install_linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install seaborn
-pip install pandas
-pip install svgutils --user
-sudo apt-get install libxml2-dev libxslt-dev
-sudo apt-get install libcairo2-devsudo
-pip install cairosvg
